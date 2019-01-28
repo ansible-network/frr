566cf909df2cfaf5174d3a8b412f27e08ed0b85c
NilashishC <nilashishchakraborty8@gmail.com>
2019-01-28 15:28:12 +0000

    Fix typo - 2
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- module_utils/network/frr/providers/cli/config/bgp/address_family.py
+++ module_utils/network/frr/providers/cli/config/bgp/address_family.py
@@ -128,7 +128,7 @@ class AddressFamily(CliProvider):
 
             cmd = 'redistribute %s' % entry['protocol']
 
-            if entry['id'] and entry['protocol'] in ('ospf', 'table'):
+            if entry['id'] and entry['protocol'] in ('ospf', 'ospf6', 'table'):
                 cmd += ' %s' % entry['id']
                 option += ' %s' % entry['id']
 
